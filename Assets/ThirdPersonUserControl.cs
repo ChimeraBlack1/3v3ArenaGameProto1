c49f2b54fe5c796ae30f8fa4a865aff2c37e6aa8
chimerablack1 <chimerablaque@hotmail.com>
2019-01-29 21:38:26 -0500

    first commit - got the camera working about 90% right

---
+++ Assets/ThirdPersonUserControl.cs
@@ -0,0 +1,97 @@
+using System;
+using UnityEngine;
+using UnityStandardAssets.CrossPlatformInput;
+
+namespace UnityStandardAssets.Characters.ThirdPerson
+{
+    [RequireComponent(typeof (ThirdPersonCharacter))]
+    public class ThirdPersonUserControl : MonoBehaviour
+    {
+        private ThirdPersonCharacter m_Character; // A reference to the ThirdPersonCharacter on the object
+        private Transform m_Cam;                  // A reference to the main camera in the scenes transform
+        private Vector3 m_CamForward;             // The current forward direction of the camera
+        private Vector3 m_Move;
+        private bool m_Jump;                      // the world-relative desired move direction, calculated from the camForward and user input.
+        public float speed = 5.0f;
+        Animator m_Animator;
+
+
+        private void Start()
+        {
+
+            m_Animator = GetComponent<Animator>();
+            // get the transform of the main camera
+            if (Camera.main != null)
+            {
+                m_Cam = Camera.main.transform;
+            }
+            else
+            {
+                Debug.LogWarning(
+                    "Warning: no main camera found. Third person character needs a Camera tagged \"MainCamera\", for camera-relative controls.", gameObject);
+                // we use self-relative controls in this case, which probably isn't what the user wants, but hey, we warned them!
+            }
+
+            // get the third person character ( this should never be null due to require component )
+            m_Character = GetComponent<ThirdPersonCharacter>();
+        }
+
+
+        private void Update()
+        {
+            if (!m_Jump)
+            {
+                m_Jump = CrossPlatformInputManager.GetButtonDown("Jump");
+            }
+
+            
+            if(Input.GetKey(KeyCode.Mouse0) && Input.GetKey(KeyCode.Mouse1))
+            {
+
+                float h = Input.GetAxis("Mouse X");
+                float v = Input.GetAxis("Mouse Y");
+                //float m_CharacterTurn = Input.GetAxis("Mouse X");
+                //Vector3 m_CharMove = new Vector3(m_CharacterTurn, 0, 0).normalized;
+                m_Animator.SetFloat("Forward", 5f, 0.01f, Time.deltaTime);
+                // calculate camera relative direction to move:
+
+                print(h+" ..."+v);
+                m_CamForward = Vector3.Scale(m_Cam.forward, new Vector3(1, 0, 1)).normalized;
+                m_Move = v * m_CamForward + h * m_Cam.right;
+                m_Character.Move(m_Move, false, false);
+            }
+
+        }
+
+
+        // Fixed update is called in sync with physics
+        private void FixedUpdate()
+        {
+            // read inputs
+            float h = CrossPlatformInputManager.GetAxis("Horizontal");
+            float v = CrossPlatformInputManager.GetAxis("Vertical");
+            bool crouch = Input.GetKey(KeyCode.C);
+
+            // calculate move direction to pass to character
+            if (m_Cam != null)
+            {
+                // calculate camera relative direction to move:
+                m_CamForward = Vector3.Scale(m_Cam.forward, new Vector3(1, 0, 1)).normalized;
+                m_Move = v*m_CamForward + h*m_Cam.right;
+            }
+            else
+            {
+                // we use world-relative directions in the case of no main camera
+                m_Move = v*Vector3.forward + h*Vector3.right;
+            }
+#if !MOBILE_INPUT
+			// walk speed multiplier
+	        if (Input.GetKey(KeyCode.LeftShift)) m_Move *= 0.5f;
+#endif
+
+            // pass all parameters to the character control script
+            m_Character.Move(m_Move, crouch, m_Jump);
+            m_Jump = false;
+        }
+    }
+}
